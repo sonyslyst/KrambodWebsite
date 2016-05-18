1600f2139fde347436b798dd562928cb070ddc2e
Asmund Berge <herrasbe@gmail.com>
2016-05-18 22:59:16 +0200

    add CKEditor with file upload

--- krambod/models.py
+++ krambod/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 
@@ -8,7 +9,7 @@ class Article(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	last_changed = models.DateTimeField(auto_now=True)
 	title = models.CharField(max_length=160)
-	content = models.CharField(max_length=1024)
+	content = RichTextUploadingField()
 	
 class Informasjon(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
--- krambodproject/urls.py
+++ krambodproject/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from krambod.views import HomePageView, InfoPageView, PhotoPageView
+from krambodproject import settings
 
 urlpatterns = [
 	url(r'^$', HomePageView.as_view(), name = "hjem"),
 	url(r'^informasjon/$', InfoPageView.as_view(), name = "informasjon"),
 	url(r'^photos/$', PhotoPageView.as_view(), name = "photos"),
     url(r'^admin/', include(admin.site.urls), name = "admin"),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
 ]
