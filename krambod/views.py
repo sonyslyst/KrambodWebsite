b732da817e9ab9b4d73c1ff0f02bce56d09b3f84
Asmund Berge <herrasbe@gmail.com>
2016-05-23 15:12:38 +0200

    filter images based on tag

--- krambod/views.py
+++ krambod/views.py
@@ -3,27 +3,41 @@ from django.views.generic.base import TemplateView
 
 from krambod.models import Article, Information, Photo, PhotoTag
 
-class HomePageView(TemplateView):
-	template_name = 'hjem.html'
-	
-	def get_context_data(self, **kwargs):
-		context = super(HomePageView, self).get_context_data(**kwargs)
-		context['articles'] = Article.objects.all()
-		return context
-		
-class InfoPageView(TemplateView):
-	template_name = 'Information.html'
-	
-	def get_context_data(self, **kwargs):
-		context = super(InfoPageView, self).get_context_data(**kwargs)
-		context['information'] = Information.objects.all()[0]
-		return context
-		
-class PhotoPageView(TemplateView):
-	template_name = 'photo.html'
+class KrambodViewMixin(object):
+    def get(self, request, *args, **kwargs):
+        self.request = request
+        return super(KrambodViewMixin, self).get(request, *args, **kwargs)
+    
 
-	def get_context_data(self, **kwargs):
-		context = super(PhotoPageView, self).get_context_data(**kwargs)
-		context['photos'] = Photo.objects.all()
-		context['tags'] = PhotoTag.objects.all()
-		return context
+class HomePageView(TemplateView):
+    template_name = 'hjem.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()
+        return context
+        
+class InfoPageView(TemplateView):
+    template_name = 'Information.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(InfoPageView, self).get_context_data(**kwargs)
+        context['information'] = Information.objects.all()[0]
+        return context
+        
+class PhotoPageView(TemplateView, KrambodViewMixin):
+    template_name = 'photo.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PhotoPageView, self).get_context_data(**kwargs)
+
+        filterByTag = self.request.GET.get('tag', None)
+        tag = None
+        try: 
+            tag = PhotoTag.objects.get(name = filterByTag)
+        except:
+            context['photos'] = Photo.objects.all()
+        else:
+            context['photos'] = Photo.objects.filter(tags = tag)
+        context['tags'] = PhotoTag.objects.all()
+        return context
